662f91973c147d8cb4bcbe0ee94d31742d645ea3
Do-it-myself <doitmyself111.engineering@gmail.com>
2022-08-26 12:35:55 +0800

    randomize msg

--- src/IOT/iot_talker.cpp
+++ src/IOT/iot_talker.cpp
@@ -1,23 +1,25 @@
 #include "ros/ros.h"
 #include "beginning_with_ros/IOTsensor.h"
 #include <stdio.h>
-
+#include <random>
 
 int main (int argc, char **argv) {
     ros::init(argc, argv, "iot_talker");
-    
     ros::NodeHandle nodeHandle;
-
     ros::Publisher publisher = nodeHandle.advertise<beginning_with_ros::IOTsensor>("iot_info", 10);
-
     ros::Rate rate(1);
+    
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<> rand_temp(0, 0.5);
+    std::uniform_real_distribution<> rand_hum(0, 0.05);
 
     while (ros::ok()) {
          beginning_with_ros::IOTsensor message;
          message.name = "IOT";
          message.id = 1;
-         message.temperature = 25.0F;
-         message.humidity = 0.8F;
+         message.temperature = 24.5F + rand_temp(gen);
+         message.humidity = 0.75F + rand_hum(gen);
 
          ROS_INFO("");
          printf("   Name: %s\n", message.name.c_str());
